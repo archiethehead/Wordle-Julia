fbb56bb3628dc00f4be9d9b3f3ab15c14575fb50
Archie Healy <archiethehead@gmail.com>
2026-02-05 23:51:13 +0000

    Random Element
    Feat: Added a supporting function that returns a random element from a given array.

--- wordle.jl
+++ wordle.jl
@@ -1,21 +1,28 @@
 word_list = [];
 
-function load_file(path::AbstractString)::vector{AbstractString}
+function load_file(path::AbstractString)::Vector{AbstractString}
 
     open(path, "r") do line
 
-        word_array = readlines(line)
+        word_list = readlines(line)
+        return word_list
 
     end
 
-    return word_array
+end
+
+function random_element(array::Vector)
+
+    index = rand((1, size(array, 1)))
+    return array[index]
 
 end
 
 function main()
 
-    word_array = load_file("words.txt");
-    print(file_names)
+    word_list = load_file("words.txt")
+    word = random_element(word_list)
+    print(word)
 
 end
 
