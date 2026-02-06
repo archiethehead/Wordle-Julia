db91c579d9b9a948ef7afdffdabd71633319079c
Archie Healy <archiethehead@gmail.com>
2026-02-06 00:48:56 +0000

    Parse
    Feat: A new parse function that looks through the guess relative to the current word, and returns an array of tuples which contain the letter and it's colour code.
    
    Feat: Implemented a main body function which orchestrates the rest of the program in a functional paradigm manner.

--- wordle.jl
+++ wordle.jl
@@ -1,5 +1,3 @@
-word_list = [];
-
 function load_file(path::AbstractString)::Vector{AbstractString}
 
     open(path, "r") do line
@@ -18,12 +16,76 @@ function random_element(array::Vector)
 
 end
 
+function check_word(guess::AbstractString, word::AbstractString)
+
+    attempt = []
+    win = true
+    for n in 1:length(word)
+
+        colour = 3
+        if guess[n] == word[n]
+            colour = 1
+
+        
+        elseif occursin(guess[n], word)
+            colour = 2
+            win = false
+        
+        else
+            win = false
+
+        end
+
+        push!(attempt, (guess[n], colour))
+
+    end
+
+    return attempt, win
+
+end
+
 function main()
 
     word_list = load_file("words.txt")
     word = random_element(word_list)
-    print(word)
+    attempts = 6
+    win = false
+
+    while attempts != 0 && win == false
+        
+        print("Guesses left = ", attempts, "\n")
+        guess = readline()
+        println("")
+
+        if length(guess) == 5
+
+            attempts -= 1
+            result, win = check_word(guess, word)
+
+            for letter in result
+
+                if letter[2] == 1
+
+                    printstyled(letter[1], color = :green)
+                
+                elseif letter[2] == 2
+
+                    printstyled(letter[1], color = :yellow)
+
+                else
+
+                    printstyled(letter[1], color = :red)
+
+                end
+
+            end
+
+        end
+
+        println("")
+
+    end
 
 end
 
-main();
+main()
